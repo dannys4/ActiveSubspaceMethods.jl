a07ad88b616fff5cec93ecc14fe6c1d0b33a47a1
Documenter.jl <documenter@juliadocs.github.io>
2025-06-05 20:04:08 +0000

    build based on 296f6d6

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#ActiveSubspaceMethods","page":"Home","title":"ActiveSubspaceMethods","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ActiveSubspaceMethods.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+[{"location":"#ActiveSubspaceMethods","page":"Home","title":"ActiveSubspaceMethods","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ActiveSubspaceMethods.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"#ActiveSubspaceMethods.ActiveSubspacesXXManopt","page":"Home","title":"ActiveSubspaceMethods.ActiveSubspacesXXManopt","text":"ActiveSubspacesXXManopt\n\nOnly usable when Manopt and Manifolds are installed.\n\n\n\n\n\n","category":"function"}]
 }

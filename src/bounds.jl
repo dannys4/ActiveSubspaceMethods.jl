68ca18978551c5a0116979c86811e57f0ff9c968
Daniel Sharp <dannys4@mit.edu>
2025-06-05 15:51:39 -0400

    Make bounds consistent using U_perp

--- src/bounds.jl
+++ src/bounds.jl
@@ -1,18 +1,20 @@
 export AS_bound, ASXX_bound
 
-function ASXX_bound(as::AbstractActiveSubspacesXX, out::ActiveSubspacesOutput, r::Int)
-    U_perp = out.vecs[:, (r + 1):end]
+function ASXX_bound(as::AbstractActiveSubspacesXX, U_perp::AbstractMatrix)
+    d,r = size(U_perp)
+    @argcheck size(as.C_AS,1) == d DimensionMismatch
     C_AS_perp = U_perp' * as.C_AS * U_perp
     C_ZZ_perp = U_perp' * as.C_ZZ * U_perp
     as_bd = tr(C_AS_perp)
     if r < as.d
-        as_bd -= 0.5 * sum(idx -> abs2(C_ZZ_perp[idx]), eachindex(C_ZZ_perp))
+        as_bd -= 0.5 * sum(abs2, C_ZZ_perp)
     end
     return as_bd
 end
 
-function AS_bound(as::AbstractActiveSubspaces, out::ActiveSubspacesOutput, r)
-    U_perp = out.vecs[:, (r + 1):end]
+function AS_bound(as::AbstractActiveSubspaces, U_perp::AbstractMatrix)
+    d = size(as.C_AS,1)
+    @argcheck size(U_perp,1) == d DimensionMismatch
     C_AS_perp = U_perp' * as.C_AS * U_perp
     return tr(C_AS_perp)
 end

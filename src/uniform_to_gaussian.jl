15ab824a87b0c5648c62bd3b878c800daaf8afd1
Daniel Sharp <dannys4@mit.edu>
2025-06-06 14:59:09 -0400

    Add utility functions

--- src/uniform_to_gaussian.jl
+++ src/uniform_to_gaussian.jl
@@ -1,43 +1,64 @@
-export GaussianizedUniformInputFunction
+export GaussianizedUniformInputFunction, GaussianizedUniformInputFunctionAD
 
 """
-    GaussianizedUniformInputFunction(fcn!, bounds; [test_eval])
+    GaussianizedUniformInputFunction([f0], f1!, bounds; [test_eval])
 Take a function that we wish to minimize L2 error w.r.t. uniform measure and transform it to be w.r.t. GaussianizedUniformInputFunction
 
 # Arguments
-- `fcn!(grad, z)::Float64` Returns function eval at `z` and puts function gradient at `z` into `grad`
+- `f0(z)::Float64` Returns function eval at `z`. Will default to using `f1!` if not provided
+- `f1!(grad, z)::Float64` Returns *same* function eval at `z` and puts function gradient at `z` into `grad`
 - `bounds::AbstractVector{<:NTuple{2}}` A sequence of (lower, upper) bounds that is as long as the number of inputs.
 - `test_eval::Bool` Whether to test function eval when constructing this functor, default `true`.
 """
-struct GaussianizedUniformInputFunction{F<:Function,V<:AbstractVector{<:NTuple{2}}}<:Function
-    f::F
+struct GaussianizedUniformInputFunction{
+    F0<:Function,F1<:Function,V<:AbstractVector{<:NTuple{2}}
+} <: Function
+    f0::F0
+    f1!::F1
     bounds::V
-    function GaussianizedUniformInputFunction(
-        f::_F, bounds::_V; test_eval=true
-    ) where {_F,_V}
-        @argcheck all(b[2] > b[1] for b in bounds)
-        test_eval && f(map(b -> (b[2] + b[1]) / 2, bounds))
-        return new{_F,_V}(f, bounds)
-    end
 end
 
-function (fcn!::GaussianizedUniformInputFunction)(x)
+function GaussianizedUniformInputFunction(
+    f1!::_F1, bounds::_V; test_eval=true
+) where {_F1,_V}
+    @argcheck all(b[2] > b[1] for b in bounds)
+    tmp_space = Vector{Float64}(undef, length(bounds))
+    test_eval && f1!(tmp_space, map(b -> (b[2] + b[1]) / 2, bounds))
+    f0 = x -> f1!(tmp_space, x)
+    return GaussianizedUniformInputFunction{typeof(f0),_F1,_V}(f0, f1!, bounds)
+end
+
+function GaussianizedUniformInputFunction(
+    f0::_F0, f1!::_F1, bounds::_V; test_eval=true
+) where {_F0,_F1,_V}
+    @argcheck all(b[2] > b[1] for b in bounds)
+    if test_eval
+        tmp_space = Vector{Float64}(undef, length(bounds))
+        tmp_pt = map(b -> (b[2] + b[1]) / 2, bounds)
+        out_f0 = f0(tmp_pt)
+        out_f1 = f1!(tmp_space, tmp_pt)
+        @assert out_f0 == out_f1
+    end
+    return GaussianizedUniformInputFunction{_F0,_F1,_V}(f0, f1!, bounds)
+end
+
+function (fcn::GaussianizedUniformInputFunction)(x)
     unif_pt = map(eachindex(fcn.bounds)) do i
         return normcdf(x[i]) * (fcn.bounds[i][2] - fcn.bounds[i][1]) + fcn.bounds[i][1]
     end
-    grad_eval = similar(unif_pt)
-    return fcn!.f(grad_eval, unif_pt), grad_eval
+    return fcn.f0(unif_pt)
 end
 
-function (fcn!::GaussianizedUniformInputFunction)(grad_eval, x)
-    unif_pt = map(eachindex(fcn!.bounds)) do dim_idx
-        return normcdf(x[dim_idx]) * (fcn!.bounds[dim_idx][2] - fcn!.bounds[dim_idx][1]) + fcn!.bounds[dim_idx][1]
+function (fcn::GaussianizedUniformInputFunction)(grad_eval, x)
+    unif_pt = map(eachindex(fcn.bounds)) do dim_idx
+        return normcdf(x[dim_idx]) * (fcn.bounds[dim_idx][2] - fcn.bounds[dim_idx][1]) +
+               fcn.bounds[dim_idx][1]
     end
-    eval_pt = fcn!.f(grad_eval, unif_pt)
-    for dim_idx in eachindex(fcn!.bounds)
+    eval_pt = fcn.f1!(grad_eval, unif_pt)
+    for dim_idx in eachindex(fcn.bounds)
         grad_eval[dim_idx] *=
-            normpdf(x[dim_idx]) * (fcn!.bounds[dim_idx][2] - fcn!.bounds[dim_idx][1]) +
-            fcn!.bounds[dim_idx][1]
+            normpdf(x[dim_idx]) * (fcn.bounds[dim_idx][2] - fcn.bounds[dim_idx][1]) +
+            fcn.bounds[dim_idx][1]
     end
     return eval_pt
 end
---
+++ src/utils.jl
@@ -0,0 +1,73 @@
+export create_Ur, MC_variance, nested_MC_err
+
+"""
+    orthogonalize(U)
+Return matrix with orthonormal columns but same range as `U`
+"""
+function orthogonalize(U)
+	qr(U).Q * Matrix(I, size(U)...)
+end
+
+"""
+    create_Ur(U_perp, [initial_guess, rng])
+Create a projector \$U_r\$ that projects onto orthogonal space to given \$U_\\perp\$.
+"""
+function create_Ur(U_perp, initial_guess=nothing, rng::AbstractRNG = Random.GLOBAL_RNG)
+    d,perp = size(U_perp)
+	@argcheck norm(I - U_perp'U_perp)/norm(I(perp)) < 1e-14
+    r = d - perp
+    A = isnothing(initial_guess) ? randn(rng, d, r) : initial_guess
+    return make_orthogonal((I - U_perp*U_perp')*A)
+end
+
+"""
+	MC_variance(fcn_eval,d,N; [rand_fcn,rng])
+Approximate the `N` sample variance of function `fcn_eval` with `d` inputs
+"""
+function MC_variance(fcn_eval,d,N;rand_fcn::F=randn,rng=Random.GLOBAL_RNG) where {F}
+	var(fcn_eval(rand_fcn(rng,d)) for _ in 1:N)
+end
+
+"""
+    nested_MC_err(fcn_eval, U_r, U_perp, N_inner, N_outer; [rand_fcn, rng])
+Calculates the conditional variance of subspaces using a pick-and-freeze nested MC method.
+
+# Arguments
+- `fcn_eval(z::Vector)::Float64` Evaluation of function taking in length-d vector
+- `U_r` a size (d,r) orthogonal matrix
+- `U_perp` a size (d,d-r) orthogonal matrix
+- `N_inner::Int` number of inner Monte Carlo samples
+- `N_outer::Int` number of outer Monte Carlo samples
+- `rand_fcn(rng,k)::Vector{Float64}` sampler of random numbers, default `randn`
+- `rng::AbstractRNG` random number generator
+"""
+function nested_MC_err(fcn_eval::Function, U_r::AbstractMatrix, U_perp::AbstractMatrix, N_inner::Int, N_outer::Int; rand_fcn::F=randn, rng::AbstractRNG = Random.GLOBAL_RNG) where {F}
+	d,r = size(U_r)
+    @argcheck size(U_perp) == (d,d-r) DimensionMismatch
+	return mean(1:N_outer) do _
+		x_outer = U_r*rand_fcn(rng, r)
+		sum_f = 0.
+		sum_sq_f = 0.
+		x = similar(x_outer)
+		for _ in 1:N_inner
+			x_inner = U_perp*rand_fcn(rng, d-r)
+			x .= x_outer + x_inner
+			f_eval = fcn_eval(x)
+			sum_f += f_eval
+			sum_sq_f += f_eval*f_eval
+		end
+		return sum_sq_f/N_inner - (sum_f/N_inner)^2
+	end
+end
+
+"""
+    GaussianizedUniformInputFunctionAD(f0, bounds, backend; [test_eval])
+Equivalent to `GaussianizedUniformInputFunction(f0, ADFunctionWrapper(f0, d, backend), bounds; test_eval)`
+"""
+function GaussianizedUniformInputFunctionAD(
+    f0::_F0, bounds::_V, backend::DifferentiationInterface.AbstractADType; test_eval=true
+) where {_F0,_V}
+    d = length(bounds)
+    f1! = ADFunctionWrapper(f0, d, backend)
+    return GaussianizedUniformInputFunction(f0, f1!, bounds; test_eval)
+end
